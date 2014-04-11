7b363e3bc09d795a893749f9227bc68fc98bbd35
Ben Chatelain <benchatelain@gmail.com>
2014-04-10 22:45:43 -0600

    Initial Commit

---
+++ CertValidate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CertValidate
+//
+//  Created by Ben Chatelain on 4/10/14.
+//  Copyright (c) 2014 @phatblat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
